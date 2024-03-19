c9de805337a0da846980ef07ea779ea18ac3e0d0
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-03-18 21:10:01 -0300

    modulo-03: Conflitos de rotas

---
+++ MyFirstApi/Comunication/Requests/RequestChangePasswordJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstApi.Comunication.Requests
+{
+    public class RequestChangePasswordJson
+    {
+        public string Currentpassword { get; set; }  = string.Empty;
+        public string  NewPassword { get; set; } = string.Empty ;
+    }
+}
--- MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/Controllers/UserController.cs
@@ -12,7 +12,7 @@ namespace MyFirstApi.Controllers
         [Route("{id}")]
         [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult Get(int id)
+        public IActionResult GetById(int id)
         {
             var result = new User
             {
@@ -43,8 +43,28 @@ namespace MyFirstApi.Controllers
 
         [HttpDelete]
         [Route("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(List<User>),StatusCodes.Status204NoContent)]
         public IActionResult Remove([FromRoute] int id)
+        {
+            return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+
+        public IActionResult GetAll()
+        {
+            var response = new List<User>()
+            {
+                new User { Id = 1, Age = 7, Name = "John"},
+                new User { Id = 2, Age = 8, Name = "Karen"}
+            };
+            return Ok(response);
+        }
+
+        [HttpPut("change-password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult ChangePassword([FromBody] RequestChangePasswordJson request)
         {
             return NoContent();
         }
