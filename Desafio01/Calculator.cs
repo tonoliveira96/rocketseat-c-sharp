adfedd4fc76b4b11665bfd700591367b748345df
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-03-19 21:49:40 -0300

    desafio-01: 3 tasks

---
+++ Desafio01/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio01
+{
+    public class Calculator
+    {
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set; }
+
+        public Calculator(double valor1, double valor2)
+        {
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+        public double Soma()
+        {
+            return Valor1 + Valor2;
+        }
+
+        public double Subtracao()
+        {
+            return Valor1 - Valor2;
+        }
+
+        public double Multiplicacao()
+        {
+            return Valor1 * Valor2;
+        }
+
+        public double Divisao()
+        {
+            return Valor1 / Valor2;
+        }
+
+        public double Media()
+        {
+            return (Valor1 + Valor2) / 2;
+        }
+    }
+}
---
+++ Desafio01/Program.cs
@@ -0,0 +1,40 @@
+﻿using Desafio01;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Task 1 - Digite seu nome para receber uma mensagem de boas vindas: ");
+            var valor = Console.ReadLine();
+            Console.WriteLine($"Olá, {valor}! Seja muito bem-vindo!");
+            Console.WriteLine("\n");
+            Console.WriteLine("**********************************************");
+
+            Console.WriteLine("Task 2 - Formar o nome conpleto com nome e sobrenome: ");
+            Console.WriteLine("Digite seu nome: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Digite seu sobrenome: ");
+            var sobrenome = Console.ReadLine();
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.WriteLine("\n");
+            Console.WriteLine("**********************************************");
+
+            Console.WriteLine("Task 2 - Operações matemáticas com 2 números: ");
+            Console.Write("Primeiro número: ");
+            var numero1 = Double.Parse(Console.ReadLine());
+            Console.Write("Segundo número: ");
+            var numero2 = Double.Parse(Console.ReadLine());
+            var calc = new Calculator(numero1, numero2);
+            Console.Write("Soma: " + calc.Soma() + "\n");
+            Console.Write("Subtração: " + calc.Subtracao() + "\n");
+            Console.Write("Multiplicação: " + calc.Multiplicacao() + "\n");
+            Console.Write("Divisão: " + calc.Divisao() + "\n");
+            Console.Write("Média: " + calc.Media() + "\n");
+            Console.WriteLine("**********************************************");
+        }
+    }
+}
+
