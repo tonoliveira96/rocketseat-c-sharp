937c6af4e4fee9c8709c108f08f6519ace1a9e6d
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-03-20 21:59:07 -0300

    desafio-02: Criado estrutura

---
+++ Desafio02/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Desafio02
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Author { get; set; } = String.Empty;
+        public string Subject { get; set; } = String.Empty;
+        public double Price { get; set; }
+        public int StoqueAmount { get; set; }
+    }
+}
---
+++ Desafio02/Controllers/BookstoreController.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookstoreController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAll()
+        {
+            return Ok(new List<Book>());
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create()
+        {
+            return Ok(new List<Book>());
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Update()
+        {
+            return Ok(new List<Book>());
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(Book), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Remove()
+        {
+            return Ok(new List<Book>());
+        }
+    }
+}
