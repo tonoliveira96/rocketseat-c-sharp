b10e014366bd4534d631ebe33fe9266237f636a7
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-04-27 15:41:39 -0300

    CashFlow: Endpoint registra despesas

---
+++ CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterExpensesJson request)
+        {
+            return Created();
+        }
+    }
+}
