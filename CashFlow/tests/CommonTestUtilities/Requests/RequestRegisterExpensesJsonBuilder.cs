1119a25484474818b600d19ff97d53a3b4448183
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-05-27 20:40:04 -0300

    CashFlow: Criando uma requisição com Bogus

---
+++ CashFlow/tests/CommonTestUtilities/Requests/RequestRegisterExpensesJsonBuilder.cs
@@ -0,0 +1,19 @@
+using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterExpensesJsonBuilder
+    {
+        public RequestRegisterExpensesJson Build()
+        {
+            return new Faker<RequestRegisterExpensesJson>()
+                .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+                .RuleFor(r => r.Date, faker => faker.Date.Past())
+                .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+                .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentsType>())
+                .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 10000));
+        }
+    }
+}
