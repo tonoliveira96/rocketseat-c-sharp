3cc747d6b7773a2c3fb0bce6b1afa25ab49b8b11
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-07-11 15:34:27 -0300

    CashFlow: Renderizando um arquivo PDF

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -2,6 +2,7 @@ using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
@@ -39,7 +40,7 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
             var totalExpenses = expenses.Sum(expenses => expenses.Amount);
             paragraph.AddFormattedText($"{totalExpenses} {CURRENCY_SYMBOL}", new Font { Name = FontHelper.WORLSANS_BLACK, Size = 50 });
 
-            return [];
+            return RenderDocument(document);
         }
 
         private Document CreateDocument(DateOnly month)
@@ -67,6 +68,21 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
             section.PageSetup.BottomMargin = 80;
 
             return section;
+        }
+
+        private byte[] RenderDocument(Document document)
+        {
+            var renderer = new PdfDocumentRenderer
+            {
+                Document = document,
+            };
+
+            renderer.RenderDocument();
+
+            using var file = new MemoryStream();
+            renderer.PdfDocument.Save(file);
+
+            return file.ToArray();
         }
     }
 }
