574e6037cc10bd925fbcfb9f55f89fad303d799e
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-07-15 09:31:47 -0300

    CashFlow: Adicionando título, cabeçalho e valor

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,3 +1,4 @@
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Colors;
 using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
@@ -39,6 +40,24 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf
             foreach (var expense in expenses)
             {
                 var table = CreateExpensesTable(page);
+                var row = table.AddRow();
+                row.Height = 25;
+
+                row.Cells[0].AddParagraph(expense.Title);
+                row.Cells[0].Format.Font = new Font { Name = FontHelper.RELEWAY_BLACK, Size = 14, Color = ColorsHelper.BLACK };
+                row.Cells[0].Shading.Color = ColorsHelper.RED_LIGHT;
+                row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+                row.Cells[0].MergeRight = 2;
+                row.Cells[0].Format.LeftIndent = 20;
+
+                row.Cells[3].AddParagraph(ResourceReportGenerationMessages.AMOUNT);
+                row.Cells[3].Format.Font = new Font { Name = FontHelper.RELEWAY_BLACK, Size = 14, Color = ColorsHelper.WHITE };
+                row.Cells[3].Shading.Color = ColorsHelper.RED_DARK;
+                row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+
+                row = table.AddRow();
+                row.Height = 30;
+                row.Borders.Visible = false;
             }
 
             return RenderDocument(document);
