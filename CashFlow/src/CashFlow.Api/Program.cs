3bdc34c02b38dd8b6cff74ec9d5b7782e417674d
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-07-26 09:17:47 -0300

    CashFlow: Executando migration pela API

--- CashFlow/src/CashFlow.Api/Program.cs
+++ CashFlow/src/CashFlow.Api/Program.cs
@@ -2,6 +2,7 @@ using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infrastructure;
+using CashFlow.Infrastructure.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,4 +35,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await MigrateDatabase();
+
 app.Run();
+
+async Task MigrateDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+
+    await DatabaseMigration.MigrationDatabase(scope.ServiceProvider);
+}
