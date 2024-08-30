cf092ef80397dde5bec658746f22c05c5b255327
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-08-30 12:28:23 -0300

    CashFlow: Utilizando Custom WebApplicationFactory

---
+++ CashFlow/tests/WebAapi.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebAapi.Test
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Test");
+        }
+    }
+}
--- CashFlow/tests/WebAapi.Test/Users/Register/RegisterUserTest.cs
+++ CashFlow/tests/WebAapi.Test/Users/Register/RegisterUserTest.cs
@@ -1,13 +1,28 @@
-using Microsoft.AspNetCore.Mvc.Testing;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using System.Net.Http.Json;
 
 namespace WebAapi.Test.Users.Register
 {
-    public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+    public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
     {
+        private const string METHOD = "api/User";
+
+        private readonly HttpClient _httpClient;
+
+        public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+
         [Fact]
         public async Task Sucess()
         {
+            var request = RequestRegisterUserJsonBuilder.Build();
 
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
         }
     }
 }
