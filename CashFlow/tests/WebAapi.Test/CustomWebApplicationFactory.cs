8e235dc48f4982ad95529527d4a8e5aba6f836c0
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-08-30 12:45:05 -0300

    CashFlow: Utilizando banco de dados InMemory

--- CashFlow/tests/WebAapi.Test/CustomWebApplicationFactory.cs
+++ CashFlow/tests/WebAapi.Test/CustomWebApplicationFactory.cs
@@ -1,5 +1,8 @@
+using CashFlow.Infrastructure.DataAccess;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebAapi.Test
 {
@@ -7,7 +10,17 @@ namespace WebAapi.Test
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.UseEnvironment("Test");
+            builder.UseEnvironment("Test")
+                .ConfigureServices(services =>
+                {
+                    var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+                    services.AddDbContext<CashFlowDbContext>(config =>
+                    {
+                        config.UseInMemoryDatabase("InMemoryDbForTesting");
+                        config.UseInternalServiceProvider(provider);
+
+                    });
+                });
         }
     }
 }
