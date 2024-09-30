38e6da90dc9eb1c932aaf724a2cf072dd8721b18
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-09-30 09:35:05 -0300

    CashFlow: Testes de unidade para recuperar perfil de usuários

---
+++ CashFlow/tests/UseCase.Test/Users/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,33 @@
+using CashFlow.Application.UseCases.Users.Profile;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repository;
+using FluentAssertions;
+
+namespace UseCase.Test.Users.Profile
+{
+    public class GetUserProfileUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var user = UserBuilder.Build();
+            var useCase = CreateUseCase(user);
+
+            var result = await useCase.Execute();
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(user.Name);
+            result.Email.Should().Be(user.Email);
+        }
+
+        private GetUserProfileUseCase CreateUseCase(User user)
+        {
+            var mapper = MapperBuilder.Build();
+            var loggedUser = LoggedUserBuilder.Build(user);
+
+            return new GetUserProfileUseCase(loggedUser, mapper);
+        }
+    }
+}
