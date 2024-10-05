a6cd17e2b57b85a9b42b1c1c0328125066a1b4d1
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-10-05 13:53:21 -0300

    CashFlow: Introdução a tags nas despesas

---
+++ CashFlow/src/CashFlow.Domain/Entities/Tag.cs
@@ -0,0 +1,10 @@
+namespace CashFlow.Domain.Entities
+{
+    public class Tag
+    {
+        public long Id { get; set; }
+        public Enums.Tag Value { get; set; }
+        public long ExpenseId { get; set; }
+        public Expense Expense { get; set; } = default!;
+    }
+}
