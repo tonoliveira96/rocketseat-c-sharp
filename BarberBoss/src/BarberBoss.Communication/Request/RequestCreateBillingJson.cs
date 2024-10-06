bc36f99a5f2cd263259c0618fa339af01f254383
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-10-06 15:06:00 -0300

    BarberBoss: Ajuste na entidade

--- BarberBoss/src/BarberBoss.Communication/Request/RequestCreateBillingJson.cs
+++ BarberBoss/src/BarberBoss.Communication/Request/RequestCreateBillingJson.cs
@@ -4,9 +4,8 @@ namespace BarberBoss.Communication.Request;
 public class RequestCreateBillingJson
 {
     public string Title { get; set; } = string.Empty;
-    public decimal value { get; set; }
+    public decimal Value { get; set; }
     public DateTime Date { get; set; }
-    public string Time { get; set; }
     public PaymentType PaymentType { get; set; }
 }
 
--- BarberBoss/src/BarberBoss.Domain/Entities/Billing.cs
+++ BarberBoss/src/BarberBoss.Domain/Entities/Billing.cs
@@ -4,6 +4,5 @@ public class Billing
     public long Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public decimal Value { get; set; }
-    public DateOnly Date { get; set; }
-    public TimeOnly Time { get; set; }
+    public DateTime Date { get; set; }
 }
